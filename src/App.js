f72c5fcefc555d2023a2c724f8532e341f0bfa7e
sarthak <sarthakofficial920@gmail.com>
2023-05-06 20:25:27 +0530

    V1-101:adding increment functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <h1>sarthak</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>increment</button>
     </div>
   )
 }
