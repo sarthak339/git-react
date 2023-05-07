6db76cc46e74a1417d9500fb927eaf33f5b33f0a
sarthak <sarthakofficial920@gmail.com>
2023-05-07 19:14:57 +0530

    K1-101 add decrement button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
     </div>
   )
 }
